df3067d13f923b5de95f9cd410630b44c1593787
Noel Ryan <noelgerard.ryan@gmail.com>
2018-10-09 10:10:54 +0200

    Added in calculator tab

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,13 +1,13 @@
+import { CalculatorPage } from '../calculator/calculator';
 import { Component } from '@angular/core';
-
 import { AboutPage } from '../about/about';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
-
-  tab2Root = AboutPage;
+    calculatorTab = CalculatorPage;
+  aboutTab = AboutPage;
 
   constructor() {
 
